9fd6c46fc2a571f434748c4ac9ebfc0d0096937a
agpavlik <agpavlik@gmail.com>
2024-03-09 18:00:30 -0500

    Add basic routes

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div />;
+  return (
+    <div>
+      <h1>Homepage</h1>
+    </div>
+  );
 }
---
+++ pages/token-topup.js
@@ -0,0 +1,7 @@
+export default function TokenTopup() {
+  return (
+    <div>
+      <h1>Token</h1>
+    </div>
+  );
+}

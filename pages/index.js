85d4e95fd30c8d931fbcb2054ea9f8d14fe002ca
agpavlik <agpavlik@gmail.com>
2024-03-09 23:31:52 -0500

    Add auth0

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
+
 export default function Home() {
+  const { user } = useUser();
+
+  console.log(user);
   return (
     <div>
-      <h1>Homepage</h1>
+      Homepage
+      <div>
+        {!!user ? (
+          <>
+            <div>
+              <Image
+                src={user.picture}
+                alt={user.name}
+                height={50}
+                width={50}
+                className="absolute"
+              />
+              <div>{user.email}</div>
+            </div>
+            <Link href="/api/auth/logout" className="btn text-left">
+              Logout
+            </Link>
+          </>
+        ) : (
+          <Link href="/api/auth/login" className="btn text-left">
+            Login
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
--- pages/post/new.js
+++ pages/post/new.js
@@ -1,7 +1,16 @@
-export default function NewPost() {
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+
+export default function NewPost(props) {
+  console.log(props);
   return (
     <div>
       <h1>NewPost</h1>
     </div>
   );
 }
+
+export const getServerSideProps = withPageAuthRequired(() => {
+  return {
+    props: {},
+  };
+});

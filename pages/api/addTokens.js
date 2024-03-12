10b24c9cbc54fed81ad7b9aa3c1a31707732a73a
agpavlik <agpavlik@gmail.com>
2024-03-12 15:49:31 -0400

    Connect MongoDB and application

--- pages/api/addTokens.js
+++ pages/api/addTokens.js
@@ -1,4 +1,5 @@
 import { getSession } from "@auth0/nextjs-auth0";
+import clientPromise from "../../lib/mongodb";
 
 export default async function handler(req, res) {
   // Logic: check to see if a user has a profile already stored in database.
@@ -7,6 +8,17 @@ export default async function handler(req, res) {
 
   // Grab the currently logged in users ID from Auth0.
   const { user } = await getSession(req, res);
-  console.log("user: ", user);
+
+  // Connect to MongoDB
+  const client = await clientPromise;
+  const db = client.db("Blogener");
+  const userProfile = await db
+    .collection("users")
+    .updateOne(
+      { auth0id: user.sub },
+      { $inc: { availableTokens: 25 }, $setOnInsert: { auth0id: user.sub } },
+      { upsert: true }
+    );
+
   res.status(200).json({ name: "J" });
 }
